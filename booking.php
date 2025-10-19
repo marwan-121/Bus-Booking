7e6bcd3ed88280303ed6808ded0b85c402f2bc3d
marwan-121 <mrwanalshafie911@gmail.com>
2025-10-19 16:54:52 +0300

    Rename booking.html to booking.php

--- booking.php
+++ booking.php
@@ -1,4 +1,4 @@
-<!--<?php
+<?php
 require_once 'config/database.php';
 require_once 'config/functions.php';
 
@@ -130,7 +130,6 @@ if (isPost()) {
     }
 }
 ?>
--->
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
